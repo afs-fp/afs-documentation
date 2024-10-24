fad986401d8f5df1b1fa818abfb573e00c127101
David Saffer <dsaffer@AFS-LT2624>
2024-10-24 19:04:34 -0400

    workaround build error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'afs-documentation'
+
 const nextConfig: NextConfig = {
-  basePath: '/afs-documentation',
+  basePath: isProd ? `/${repoName}` : '',
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
+  images: { unoptimized: true },
 }
 
 export default nextConfig
